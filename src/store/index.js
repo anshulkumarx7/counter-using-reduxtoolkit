2af72c2470c38a5c5ac4571eb6cbc58da0727773
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-07-31 20:53:02 +0530

    first{

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { configureStore,createSlice } from "@reduxjs/toolkit";
+
+const counterSlice=createSlice({
+    name:"counter",
+    initialState:{counter:0},
+    reducers:{
+        increment(state,action){
+            state.counter++;
+        },
+        decrement(state,action){
+            state.counter--;
+        },
+        reset(state,action){
+            state.counter=0;
+        },
+        addBy10(state,action){
+            state.counter+=action.payload;
+        }
+    }
+});
+export const actions=counterSlice.actions;
+const store=configureStore({
+    reducer:counterSlice.reducer,
+})
+export default store;
